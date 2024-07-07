9522aeaa1d1253a053cb66fe4653e6ab04171919
Alphonsus Gabriel <84717082+Alphosmart@users.noreply.github.com>
2024-07-07 18:35:31 +0100

    Initial commit

---
+++ .history/script_20240613231141.js
@@ -0,0 +1,9 @@
+function gradeStudent() { const score =
+parseInt(document.getElementById('score').value); const resultElement =
+document.getElementById('result'); let grade = ''; let sound = ''; if (score >=
+90) { grade = 'A'; sound = document.getElementById('soundA'); } else if (score
+>= 80) { grade = 'B'; sound = document.getElementById('soundB'); } else if
+(score >= 70) { grade = 'C'; sound = document.getElementById('soundC'); } else
+if (score >= 60) { grade = 'D'; sound = document.getElementById('soundD'); }
+else { grade = 'F'; sound = document.getElementById('soundF'); }
+resultElement.textContent = `The student's grade is: ${grade}`; sound.play(); }
---
+++ .history/script_20240614135945.js
@@ -0,0 +1,24 @@
+function gradeStudent() {
+  const score = parseInt(document.getElementById("score").value);
+  const resultElement = document.getElementById("result");
+  let grade = "";
+  let sound = "";
+  if (score >= 70 && score <= 100) {
+    grade = "Excellent";
+    sound = document.getElementById("soundA");
+  } else if (score >= 80) {
+    grade = "Very Good";
+    sound = document.getElementById("soundB");
+  } else if (score >= 70) {
+    grade = "Credit";
+    sound = document.getElementById("soundC");
+  } else if (score >= 60) {
+    grade = "Pass";
+    sound = document.getElementById("soundD");
+  } else {
+    grade = "Fail";
+    sound = document.getElementById("soundF");
+  }
+  resultElement.textContent = `Your overall result is ${grade}`;
+  sound.play();
+}
